92d2881b495f1bcc245cb76f6b641e4bc6f4cb85
cfigueirasvalverde <cfigueirasvalverde@nautilus26.danielcastelao.org>
2019-01-16 12:04:20 +0100

    Boletin_18

---
+++ src/boletin_18_1/Boletin_18.java
@@ -0,0 +1,49 @@
+package boletin_18_1;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin_18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public class Persoal {
+    private String telefono;
+    private String correo;
+
+    public Persoal() {
+    }
+
+    public Persoal(String telefono, String correo) {
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "telefono=" + telefono + ", correo=" + correo;
+    }
+    
+    
+    
+}
+    }
+    
---
+++ src/boletin_18_2/Aplicacion.java
@@ -0,0 +1,26 @@
+package boletin_18_2;
+
+import boletin_18_1.Boletin_18.Persoal;
+
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persoal gustavo = new Persoal("615728435", "gustagus@vamoadarle.org");
+        Persoal roberto = new Persoal("659843147", "bertoroberto@gmail.com");
+        Academica gustavo = new Academica("Gustavo", 19, gustavo);
+        Academica roberto = new Academica("Roberto", 16, roberto);
+        gustavo.setNota(8);
+        System.out.println(gustavo.toString());
+        System.out.println(roberto.toString());
+    }
+    
+}
